8fdb7a6c42a42cd599f143b44bd0a85440973497
Leonel Zeballos Aldunate <zeballos.leonel@outlook.com>
2025-08-14 20:22:07 -0400

    linux lag solved

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,10 @@ import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
+		String os = System.getProperty("os.name").toLowerCase();
+		if (os.contains("linux")) {
+			System.setProperty("sun.java2d.opengl", "true");
+		}
 		JFrame window = new JFrame();
 		GamePanel gamePanel = new GamePanel();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
